b77365639d4cfc901c13ceaba644286609f88fa1
alejandro.domingo.ga <mralexsito@gmail.com>
2025-12-15 19:32:31 +0100

    data station information api hecha

--- lib/data/station_information_Api.dart
+++ lib/data/station_information_Api.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class SimpleApi {
+class station_information_Api {
   static const String _base='https://acoruna.publicbikesystem.net/customer/gbfs/v2/gl/station_information';
 
   Future<List<dynamic>> getPostsJson() async {
-    final url= Uri.parse('$_base/posts');
+    final url = Uri.parse(_base);
     final res = await http.get(url);
     
     if (res.statusCode != 200) {
