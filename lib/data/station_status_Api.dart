cbb3b89676424ae5fbc02d522ca5ec2d5889b131
alejandro.domingo.ga <mralexsito@gmail.com>
2025-12-15 19:39:22 +0100

    data station status teminada? no lo se :)

---
+++ lib/data/station_status_Api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class station_status_Api {
+  static const String _base ='https://acoruna.publicbikesystem.net/customer/gbfs/v2/gl/station_status';
+  
+  Future<List<dynamic>> getPostsJson() async {
+    final url = Uri.parse(_base);
+    final res = await http.get(url);
+
+    if(res.statusCode!= 200){
+      throw Exception('HTTP ${res.statusCode}');
+    }
+
+    final decoded = jsonDecode(res.body);
+
+    if(decoded is! List){
+      throw Exception('Respuesta inesperada');
+    }
+
+    return decoded;
+  }
+}
